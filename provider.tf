57d03986da3d3a96f9e7df3056ab1dddfbe4bce1
Sivaaws87 <95564099+Sivaaws87@users.noreply.github.com>
2021-12-06 13:48:29 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
 
-  access_key = "AKIA3NEIYKDO2MDHZUBD"
+  access_key = "AKIA3NEIYKDO6GPLTTO6"
 
-  secret_key = "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"
+  secret_key = "pc4v3OGAn8WfV/rd8GgzIomEAKRRVGxov8dQH/aN"
 
   region     = "us-west-2"
 }
