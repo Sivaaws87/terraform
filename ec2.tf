069dac1c00be90f9c7f5d0b5f2524a56789310b2
Sivaaws87 <95564099+Sivaaws87@users.noreply.github.com>
2021-12-08 15:49:34 +0000

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "role-test" {
+  ami = "ami-0d37e07bd4ff37148"
+  instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
+  key_name = "aws-test"
+  user_data = <<-EOL
+  #!/bin/bash -xe
+  sudo yum update -y
+  sudo yum install -y awslogs
+  sudo yum install -y git
+  git clone https://github.com/arvindpathare/terraform-test.git
+  sudo systemctl start awslogsd
+  sudo systemctl enable awslogsd.service
+  sudo amazon-linux-extras install docker -y
+  sudo yum install docker -y 
+  sudo service docker start
+  sudo usermod -a -G docker ec2-user
+  docker build -t mynginxterra .
+  docker run --name foo -d -p 8080:80 mynginx
+  aws s3 cp s3://s3-terraform-bucket-srk/index.html /home/ec2-user
+  
+  EOL
+  tags = {
+    Name = "Terratest"
+  }
+}
